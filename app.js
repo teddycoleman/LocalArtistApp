80d3d592454a73f5169d4233b1d8954c88b7b0c5
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-20 11:45:24 -0800

    adds intial page and working modal

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module("userApp", ["ngRoute"])
+var app = angular.module("localArtist", ['ngRoute','ngAnimate','ngTouch','ui.bootstrap']); 
 
 app.config(function($routeProvider) {
 	$routeProvider
@@ -12,4 +12,40 @@ app.config(function($routeProvider) {
 		});
 });
 
+
+app.controller("HomepageController", function ($scope, $uibModal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function () {
+
+    var modalInstance = $uibModal.open({
+      templateUrl: 'templates/myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      scope: $scope,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
 
