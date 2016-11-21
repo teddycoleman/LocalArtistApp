cd8ea1a14e0fea96666b57bd0eedcbbf4c3f45e7
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-21 11:47:22 -0800

    adds form to create profile requests and hits API request to complete creation

--- controllers/CreateProfileController.js
+++ controllers/CreateProfileController.js
@@ -2,6 +2,22 @@ angular
   .module('localArtist')
   .controller('CreateProfileController', CreateProfileController);
 
-function CreateProfileController () {
+function CreateProfileController ($scope, $http, $location) {
 
+	$scope.create_profile = function (){
+		console.log($scope.profile);
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/profiles.json',
+      data: {profile: $scope.profile},
+      headers: {Authorization: "Token token=" + sessionStorage.getItem("auth_token")}
+    }).success(function (profile){
+      console.log(profile);
+    }).error(function(error) {
+      alert(error);
+    });
+	}
+	$scope.cancel = function() {
+		$location.path("/");
+	}
 };
