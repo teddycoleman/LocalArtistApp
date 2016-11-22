a663e5c60b95dca820229a8d851606ce73a5cdb5
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-21 21:16:54 -0800

    adds galleria to display set of photos

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -4,4 +4,9 @@ angular
 
 function ProfileController ($scope, $http, $location) {
 	console.log("OK");
+
+	(function() { 
+          Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.4.5/themes/classic/galleria.classic.min.js');
+          Galleria.run('.galleria');
+      }());
 }
