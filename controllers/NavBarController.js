61e91246b9f21ef3bc18bb51ea909ebedb3535ee
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-27 11:33:53 -0800

    adds page reload on sign in and logout

--- controllers/NavBarController.js
+++ controllers/NavBarController.js
@@ -1,4 +1,4 @@
-function NavBarController ($scope, $uibModal, $log, $http, $location) {
+function NavBarController ($scope, $uibModal, $log, $http, $location, $route) {
 
   $scope.openSignUp = function () {
 
@@ -51,6 +51,7 @@ function NavBarController ($scope, $uibModal, $log, $http, $location) {
         }).success(function (user){
           sessionStorage.setItem("auth_token", user.auth_token);
           sessionStorage.setItem("user_id", user.id);
+          $route.reload();
         }).error(function(error) {
           Alert("Error creating user!");
         });
@@ -61,6 +62,7 @@ function NavBarController ($scope, $uibModal, $log, $http, $location) {
   $scope.logout = function(){
     sessionStorage.removeItem("auth_token");
     sessionStorage.removeItem("user_id");
+    $route.reload();
   }
 
   $scope.goToCreateProfile = function(){
