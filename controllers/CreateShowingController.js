c719a99e2768cdbb0403907a0bdd65f83bf2b5e9
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-27 09:53:32 -0800

    updates create showing form to properly show available artwork to show

--- controllers/CreateShowingController.js
+++ controllers/CreateShowingController.js
@@ -1,7 +1,7 @@
-function CreateShowingController ($scope, $http, $location) {
+function CreateShowingController ($scope, $http, $location, $q) {
 
   $scope.profileId = $location.path().split('/')[2];
-  console.log($scope.profileId);
+  $scope.filterId = null;
 
   $scope.data = {
     selectedProfile: null,
@@ -9,32 +9,75 @@ function CreateShowingController ($scope, $http, $location) {
     description: null
   }
 
-  $http({
-    method: 'GET',
-    url: 'http://localhost:3000/profiles/' + $scope.profileId
-  }).success(function (main_profile){
-    $scope.main_profile = main_profile;
-  }).error(function(error) {
-    console.log(error);
-  });
+  $scope.getMainProfile = function() {
+    var deferred = $q.defer();
 
-  $http({
-    method: 'GET',
-    url: 'http://localhost:3000/profiles/' 
-  }).success(function (profiles){
-    $scope.profiles = profiles;
-  }).error(function(error) {
-    console.log(error);
-  });
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/profiles/' + $scope.profileId
+    }).success(function (main_profile){
+      $scope.main_profile = main_profile;
+      if(main_profile[0].profile_type === 'artist') {
+        $scope.filterId = $scope.profileId;
+      }
+      deferred.resolve(main_profile);
+    }).error(function(error) {
+      console.log(error);
+    });
 
-  $http({
-    method: 'GET',
-    url: 'http://localhost:3000/profiles/' + $scope.profileId + '/photos'
-  }).success(function (photos){
-    $scope.photos = photos
-  }).error(function(error) {
-    console.log(error);
-  });
+    return deferred.promise;
+  }
+
+  $scope.getAllProfiles = function() {
+    var deferred = $q.defer();
+
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/profiles/' 
+    }).success(function (profiles){
+      $scope.profiles = profiles;
+      deferred.resolve(profiles);
+    }).error(function(error) {
+      console.log(error);
+    });
+
+    return deferred.promise;
+  }
+
+  $scope.populatePhotos = function() { 
+    console.log($scope.photos);
+    if($scope.main_profile[0].profile_type === 'artist'){
+      console.log("getting own photos")
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/profiles/' + $scope.profileId + '/photos'
+      }).success(function (photos){
+        $scope.photos = photos
+      }).error(function(error) {
+        console.log(error);
+      });
+    }
+    else {
+      console.log("getting other photos");
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/photos'
+      }).success(function (photos){
+        $scope.photos = photos
+        console.log(photos);
+      }).error(function(error) {
+        console.log(error);
+      });
+    }
+  }
+
+  $q.all([
+    $scope.getMainProfile(),
+    $scope.getAllProfiles()
+  ]).then(function(data) {
+      $scope.populatePhotos();
+    }
+  );
 
   $scope.create_showing = function(){
     
@@ -82,3 +125,4 @@ function CreateShowingController ($scope, $http, $location) {
     return bool;
   }
 };
+
