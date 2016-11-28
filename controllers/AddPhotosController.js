20b85d1287451e2723bd8de4734551d859ec7cb9
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-28 10:45:29 -0800

    fixes inconsistent tabbing

--- controllers/AddPhotosController.js
+++ controllers/AddPhotosController.js
@@ -1,40 +1,40 @@
 function AddPhotosController ($scope, $http, $location, Upload) {
-    var profileId = $location.path().split("/")[1];
-    $scope.images = [{url:"https://files.graphiq.com/2307/media/images/White_430113_i0.png"}];
-    var imageLoaded = false;
+  var profileId = $location.path().split("/")[1];
+  $scope.images = [{url:"https://files.graphiq.com/2307/media/images/White_430113_i0.png"}];
+  var imageLoaded = false;
 
-    $scope.uploadFiles = function(files, errFiles) {
-        $scope.file = files;
-        $scope.errFiles = errFiles;
+  $scope.uploadFiles = function(files, errFiles) {
+    $scope.file = files;
+    $scope.errFiles = errFiles;
 
-        Upload.upload({
-            url: 'http://localhost:3000/profiles/' + profileId + '/photos.json',
-            data: {
-                photo: files
-            },
-            headers: {
-                Authorization: "Token token=" + sessionStorage.getItem("auth_token")
-            }
-        })
-        .then(function (photo){
-            if(!imageLoaded){
-                imageLoaded = true;
-                $scope.images = [];
-            }
-            $scope.images.push({
-                url: 'http://localhost:3000' + photo.data[1]
-            });
-            $scope.methods.next();
-        }, function(resp) {
-            console.log(resp);
-        });
-    }
+    Upload.upload({
+      url: 'http://localhost:3000/profiles/' + profileId + '/photos.json',
+      data: {
+        photo: files
+      },
+      headers: {
+        Authorization: "Token token=" + sessionStorage.getItem("auth_token")
+      }
+    })
+    .then(function (photo){
+      if(!imageLoaded){
+        imageLoaded = true;
+        $scope.images = [];
+      }
+      $scope.images.push({
+        url: 'http://localhost:3000' + photo.data[1]
+      });
+      $scope.methods.next();
+    }, function(resp) {
+      console.log(resp);
+    });
+  }
 
-    $scope.cancel = function() {
-        $location.path("/");
-    }
+  $scope.cancel = function() {
+    $location.path("/");
+  }
 
-    $scope.goToProfile = function() {
-        $location.path("/profiles/" + profileId);
-    }
+  $scope.goToProfile = function() {
+    $location.path("/profiles/" + profileId);
+  }
 };
--- controllers/CreateProfileController.js
+++ controllers/CreateProfileController.js
@@ -1,37 +1,37 @@
 function PhotoUploadService(Upload, $location) {
-    this.upload = function(profile) {
-        Upload.upload({
-            url: 'http://localhost:3000/profiles.json',
-            data: {
-                profile: {
-                    profile_pic: profile.profile_pic,
-                    name: profile.name,
-                    style: profile.style,
-                    profile_type: profile.profile_type,
-                    description: profile.description,
-                    user_id: sessionStorage.getItem("user_id")
-                }
-            },
-            headers: {
-                Authorization: "Token token=" + sessionStorage.getItem("auth_token")
-            }
-        })
-        .then(function (profile){
-            $location.path("/" + profile.data.id + "/add_photos");
-        }, function(resp) {
-            console.log(resp);
-        });
-    }
+  this.upload = function(profile) {
+    Upload.upload({
+      url: 'http://localhost:3000/profiles.json',
+      data: {
+        profile: {
+          profile_pic: profile.profile_pic,
+          name: profile.name,
+          style: profile.style,
+          profile_type: profile.profile_type,
+          description: profile.description,
+          user_id: sessionStorage.getItem("user_id")
+        }
+      },
+      headers: {
+        Authorization: "Token token=" + sessionStorage.getItem("auth_token")
+      }
+    })
+    .then(function (profile){
+      $location.path("/" + profile.data.id + "/add_photos");
+    }, function(resp) {
+      console.log(resp);
+    });
+  }
 }
 
 function CreateProfileController ($scope, $http, $location, PhotoUploadService) {
-    console.log("here");
-    $scope.create_profile = function (){
-        console.log("also here");
-        PhotoUploadService.upload($scope.profile);
-    }
-    
-    $scope.cancel = function() {
-        $location.path("/");
-    }
+  console.log("here");
+  $scope.create_profile = function (){
+    console.log("also here");
+    PhotoUploadService.upload($scope.profile);
+  }
+  
+  $scope.cancel = function() {
+    $location.path("/");
+  }
 };
